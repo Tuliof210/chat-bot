d4b9820649ed19840c502de1861909936a5a7aef
Tulio Ferreira <tuliof210@gmail.com>
2021-03-27 13:15:54 -0300

    install typescript

---
+++ src/app/layouts/Main.tsx
@@ -0,0 +1,12 @@
+// Core imports
+import React from 'react';
+// Style imports
+import './Main.scss';
+
+export default (props: any) => {
+  return (
+    <div>
+      <h1>Olas</h1>
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// Core imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Style imports
+import './index.scss';
+// Custom imports
+import Main from './app/layouts/Main';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
